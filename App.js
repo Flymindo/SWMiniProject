dbef0661ff8d0a930596f1b9dee8a323c7fb3cba
Flymindo <64186183+Flymindo@users.noreply.github.com>
2021-09-07 17:10:22 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import Axios from 'axios'
+import React, {useState} from 'react'
+// import { RNCamera, FaceDetector } from 'react-native-camera';
+
+function App() {
+  const express = require('express')
+  const server = express()
+  // const {calory,searchCalory} = useState('')
+  const API_KEY = '1STbT8Zsp6d9CcLirJjDRE9UoS6aklojen8h5que';
+  const url = "https://api.nal.usda.gov/fdc/v1/food/11090?api_key=" + API_KEY;
+  
+  const getCalories  = () => {
+    // Axios.get(url).then ( (response) => {
+    //   response.json();
+    //   // console.log(result)
+    // }).then( (data) => {
+    //   console.log(data);
+    //   return;
+    // })
+    server.get(url)
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        Get your food nutrient info
+      </header>
+      <body className = "App-body">
+        <button onClick = {getCalories}>
+          click
+        </button>
+        {/* <p>
+          {getCalories}
+        </p> */}
+        <textarea>
+          Enter a name of a food you are looking for
+        </textarea>
+        <button>
+          Scan a barcode
+        </button>
+      </body>
+    </div>
+  );
+}
+
+export default App;
