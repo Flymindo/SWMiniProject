f921d26468821a1d16e17864981a05dd2a3cd3c7
Flymindo <64186183+Flymindo@users.noreply.github.com>
2021-09-12 23:11:56 -0400

    Add files via upload

--- src/screens/Scan.js
+++ src/screens/Scan.js
@@ -1,22 +1,134 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
-const Scan = ({navigation}) => {
-    return(
-        <View style = {styles.home}>
-            <Text> Barcode Scan </Text>
-            <TouchableOpacity onPress = { () => navigation.navigate('Home')}>
-                <Text>Go Back to Home</Text>
-            </TouchableOpacity>  
-        </View>
-    )
+class Scan extends Component {
+
+  constructor(props) {
+    super(props);
+    this.camera = null;
+    this.barcodeCodes = [];
+
+    this.state = {
+      camera: {
+        type: RNCamera.Constants.Type.back,
+	flashMode: RNCamera.Constants.FlashMode.auto,
+      }
+    };
+  }
+
+  onBarCodeRead(scanResult) {
+    // console.warn(scanResult.type);
+    // console.warn(scanResult.data);
+    if (scanResult.data != null) {
+	if (!this.barcodeCodes.includes(scanResult.data)) {
+	  this.barcodeCodes.push(scanResult.data);
+      console.log(this.barcodeCodes)
+	//   console.warn('onBarCodeRead call');
+	}
+    }
+    return;
+  }
+
+  async takePicture() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  }
+
+  pendingView() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'lightgreen',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text>Waiting</Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            defaultTouchToFocus
+            flashMode={this.state.camera.flashMode}
+            mirrorImage={false}
+            onBarCodeRead={this.onBarCodeRead.bind(this)}
+            onFocusChanged={() => {}}
+            onZoomChanged={() => {}}
+            style={styles.preview}
+            type={this.state.camera.type}
+        />
+        <View style={[styles.overlay, styles.topOverlay]}>
+	  <Text style={styles.scanScreenMessage}>Please scan the barcode.</Text>
+	</View>
+	<View style={[styles.overlay, styles.bottomOverlay]}>
+          <Button
+            onPress={() => {
+                console.log('scan clicked');
+                //insert getData() function from routes/getNutrients.js
+                }}
+            style={styles.enterBarcodeManualButton}
+            title="Enter Barcode"
+           />
+	</View>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-    home:{
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+const styles = {
+  container: {
+    flex: 1
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  overlay: {
+    position: 'absolute',
+    padding: 16,
+    right: 0,
+    left: 0,
+    alignItems: 'center'
+  },
+  topOverlay: {
+    top: 0,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  bottomOverlay: {
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  enterBarcodeManualButton: {
+    padding: 15,
+    backgroundColor: 'white',
+    borderRadius: 40
+  },
+  scanScreenMessage: {
+    fontSize: 14,
+    color: 'white',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+};
+
 export default Scan;
